f9cdf74838f8864caead2ff092b648e09ae055c7
Ryan Oldford <ryan.oldford@gmail.com>
2018-11-01 13:33:46 +0800

    Removed unneeded UI files

--- spec/pronoun-swapper-view-spec.js
+++
@@ -1,9 +0,0 @@
-'use babel';
-
-import PronounSwapperView from '../lib/pronoun-swapper-view';
-
-describe('PronounSwapperView', () => {
-  it('has one valid test', () => {
-    expect('life').toBe('easy');
-  });
-});
